d600f8656fb83af944db22e09a6be521589d71f4
SaimonMR <simon.martinez.rzs@gmail.com>
2024-04-16 19:04:15 -0400

    first commit

---
+++ Installations/installation_ros_noetic.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# General stup NUC for marsupial agents
+
+echo "Calling UPDATE \n\n"
+
+sudo apt update
+
+## Install ROS Noetic
+echo "\n\n INSTALLING ROS_NOETIC \n\n"
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install curl # if you haven't already installed curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update 
+sudo apt install ros-noetic-desktop-full
+source /opt/ros/noetic/setup.bash
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install python3-rosdep
+sudo rosdep init
+rosdep update
